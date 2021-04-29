c4b659f6a2f9ff4136b83bccf6d4fd2596956e9d
sumanth <sumanthku17@gmail.com>
2021-04-29 12:54:37 +0530

    obj array usage

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state =
     {
-        count : 0
+        count : 1,
+        tag : ['tag1','tag2','tag3']
     };
     styles =
     {
@@ -11,12 +12,25 @@ class Counter extends Component {
         fontSize : 20
     };
     render() { 
+
+        //let classs = this.badgeColorProvision();
+
         return (
+
         <React.Fragment> 
-            <span style={this.styles} className="badge badge-primary m-2">{this.myfunc()}</span>
+            <span style={this.styles} className={this.badgeColorProvision()}>{this.myfunc()}</span>
             <button className="btn btn-secondary btn-sm m-2">my</button> 
-        </React.Fragment>
+            <ul>
+                {this.state.tag.map(tag => <li key= {tag}>{tag}</li>)}      
+            </ul>
+        </React.Fragment> // key need to be specified to take out react warning 
         );
+    }
+
+    badgeColorProvision() {
+        let classs = "badge m-2 badge-";
+        classs += this.co === 0 ? "warning" : "primary";
+        return classs;
     }
 
     myfunc()
