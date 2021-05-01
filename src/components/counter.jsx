0f44f7795c461d0cb934300dad6540d970ccbe3e
sumanth <sumanthku17@gmail.com>
2021-05-01 10:54:44 +0530

    using setState()-incrementing acc to clicks

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state =
     {
-        count : 1
+        count : 0
     };
 
     // constructor()
@@ -15,7 +15,8 @@ class Counter extends Component {
 
     handleInc = () =>
     {
-        console.log('inc clicked',this);
+        //console.log('inc clicked',this);
+        this.setState({count : this.state.count+1})
     }
     
     render() { 
@@ -24,10 +25,25 @@ class Counter extends Component {
         return (
 
         <React.Fragment> 
+            <span className={this.getBadge()}>{this.counting()}</span>
             <button onClick={this.handleInc} className="btn btn-secondary btn-sm">mine</button>
         </React.Fragment> // handling prob of this pointing to window or undef in two ways 1. using construnctor and bind method 2. using arrow func
         );
     }
+
+    counting()
+    {
+        return this.state.count === 0 ? "zero" : this.state.count;
+    }
+
+    getBadge()
+    {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary"
+        return classes;
+    }
+
+
 
 
 }
