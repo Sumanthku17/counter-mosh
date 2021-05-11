356c75f652f7709f043ced191f9347e776653dea
sumanth <sumanthku17@gmail.com>
2021-05-12 03:10:00 +0530

    StateLess Functional Component[SFC]

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ const navBar = (props) => {// if u don't have any states or methods defined in a
         <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
             <a className="navbar-brand" href="#">
-                Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCount}</span>
+                Navbar <span className="badge badge-pill badge-secondary">{props.totalCount}</span>
             </a>
         </div>
         </nav>
