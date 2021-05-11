474d39fcc6144c8753fa36634080590227825bf5
sumanth <sumanthku17@gmail.com>
2021-05-12 03:07:25 +0530

    StateLess Functional Component[SFC]

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <nav className="navbar navbar-light bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCount}</span>
-                </a>
-            </div>
-            </nav>
-         );
-    }
+const navBar = (props) => {// if u don't have any states or methods defined in a comp.. u can change it to StateLess Functional Component[SFC].
+    return ( 
+         
+        <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+                Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCount}</span>
+            </a>
+        </div>
+        </nav>
+         
+     );
 }
  
-export default NavBar;
+export default navBar;
